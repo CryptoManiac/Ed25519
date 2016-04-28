a1ffb5d4d291c448ffbef25496f26e73d67c135e
CryptoManiac <balthazar.ad@gmail.com>
2016-04-28 13:22:17 +0300

    Initial commit

---
+++ Chaos.NaCl.Tests/Ed25519Tests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chaos.NaCl.Tests
+{
+    [TestClass]
+    public class Ed25519Tests
+    {
+        [AssemblyInitializeAttribute]
+        public static void LoadTestVectors(TestContext context)
+        {
+            Ed25519TestVectors.LoadTestCases();
+            //Warmup
+            var pk = Ed25519.PublicKeyFromSeed(new byte[32]);
+            var sk = Ed25519.ExpandedPrivateKeyFromSeed(new byte[32]);
+            var sig = Ed25519.Sign(Ed25519TestVectors.TestCases.Last().getMessage(), sk);
+            Ed25519.Verify(sig, new byte[10], pk);
+        }
+
+        [TestMethod]
+        public void KeyPairFromSeed()
+        {
+            foreach (var testCase in Ed25519TestVectors.TestCases)
+            {
+                byte[] publicKey;
+                byte[] privateKey;
+                Ed25519.KeyPairFromSeed(out publicKey, out privateKey, testCase.getSeed());
+                TestHelpers.AssertEqualBytes(testCase.getPublicKey(), publicKey);
+                TestHelpers.AssertEqualBytes(testCase.getPrivateKey(), privateKey);
+            }
+        }
+
+
+        [TestMethod]
+        public void KeyPairFromSeedSegments()
+        {
+            foreach (var testCase in Ed25519TestVectors.TestCases)
+            {
+                var publicKey = new byte[Ed25519.PublicKeySize].Pad();
+                var privateKey = new byte[Ed25519.ExpandedPrivateKeySize].Pad();
+                Ed25519.KeyPairFromSeed(publicKey, privateKey, testCase.getSeed().Pad());
+                TestHelpers.AssertEqualBytes(testCase.getPublicKey(), publicKey.UnPad());
+                TestHelpers.AssertEqualBytes(testCase.getPrivateKey(), privateKey.UnPad());
+            }
+        }
+
+        [TestMethod]
+        public void Sign()
+        {
+            foreach (var testCase in Ed25519TestVectors.TestCases)
+            {
+                var sig = Ed25519.Sign(testCase.getMessage(), testCase.getPrivateKey());
+                Assert.AreEqual(64, sig.Length);
+                TestHelpers.AssertEqualBytes(testCase.getSignature(), sig);
+            }
+        }
+
+        [TestMethod]
+        public void Verify()
+        {
+            foreach (var testCase in Ed25519TestVectors.TestCases)
+            {
+                bool success = Ed25519.Verify(testCase.getSignature(), testCase.getMessage(), testCase.getPublicKey());
+                Assert.IsTrue(success);
+            }
+        }
+
+        [TestMethod]
+        public void VerifyFail()
+        {
+            var message = Enumerable.Range(0, 100).Select(i => (byte)i).ToArray();
+            byte[] pk;
+            byte[] sk;
+            Ed25519.KeyPairFromSeed(out pk, out sk, new byte[32]);
+            var signature = Ed25519.Sign(message, sk);
+            Assert.IsTrue(Ed25519.Verify(signature, message, pk));
+            foreach (var modifiedMessage in message.WithChangedBit())
+            {
+                Assert.IsFalse(Ed25519.Verify(signature, modifiedMessage, pk));
+            }
+            foreach (var modifiedSignature in signature.WithChangedBit())
+            {
+                Assert.IsFalse(Ed25519.Verify(modifiedSignature, message, pk));
+            }
+        }
+
+        private byte[] AddL(IEnumerable<byte> input)
+        {
+            var signedInput = input.Concat(new byte[] { 0 }).ToArray();
+            var i = new BigInteger(signedInput);
+            var l = BigInteger.Pow(2, 252) + BigInteger.Parse("27742317777372353535851937790883648493");
+            i += l;
+            var result = i.ToByteArray().Concat(Enumerable.Repeat((byte)0, 32)).Take(32).ToArray();
+            return result;
+        }
+
+        private byte[] AddLToSignature(byte[] signature)
+        {
+            return signature.Take(32).Concat(AddL(signature.Skip(32))).ToArray();
+        }
+
+        // Ed25519 is malleable in the `S` part of the signature
+        // One can add (a multiple of) the order of the subgroup `l` to `S` without invalidating the signature
+        // The implementation only checks if the 3 high bits are zero, which is equivalent to checking if S < 2^253
+        // since `l` is only slightly larger than 2^252 this means that you can add `l` to almost every signature
+        // *once* without violating this condition, adding it twice will exceed 2^253 causing the signature to be rejected
+        // This test serves to document the *is* behaviour, and doesn't define *should* behaviour
+        //
+        // I consider rejecting signatures with S >= l, but should probably talk to upstream and libsodium before that
+        [TestMethod]
+        public void MalleabilityAddL()
+        {
+            var message = Enumerable.Range(0, 100).Select(i => (byte)i).ToArray();
+            byte[] pk;
+            byte[] sk;
+            Ed25519.KeyPairFromSeed(out pk, out sk, new byte[32]);
+            var signature = Ed25519.Sign(message, sk);
+            Assert.IsTrue(Ed25519.Verify(signature, message, pk));
+            var modifiedSignature = AddLToSignature(signature);
+            Assert.IsTrue(Ed25519.Verify(modifiedSignature, message, pk));
+            var modifiedSignature2 = AddLToSignature(modifiedSignature);
+            Assert.IsFalse(Ed25519.Verify(modifiedSignature2, message, pk));
+        }
+
+        [TestMethod]
+        public void VerifySegments()
+        {
+            foreach (var testCase in Ed25519TestVectors.TestCases)
+            {
+                bool success = Ed25519.Verify(testCase.getSignature().Pad(), testCase.getMessage().Pad(), testCase.getPublicKey().Pad());
+                Assert.IsTrue(success);
+            }
+        }
+
+        [TestMethod]
+        public void VerifyFailSegments()
+        {
+            var message = Enumerable.Range(0, 100).Select(i => (byte)i).ToArray();
+            byte[] pk;
+            byte[] sk;
+            Ed25519.KeyPairFromSeed(out pk, out sk, new byte[32]);
+            var signature = Ed25519.Sign(message, sk);
+            Assert.IsTrue(Ed25519.Verify(signature.Pad(), message.Pad(), pk.Pad()));
+            foreach (var modifiedMessage in message.WithChangedBit())
+            {
+                Assert.IsFalse(Ed25519.Verify(signature.Pad(), modifiedMessage.Pad(), pk.Pad()));
+            }
+            foreach (var modifiedSignature in signature.WithChangedBit())
+            {
+                Assert.IsFalse(Ed25519.Verify(modifiedSignature.Pad(), message.Pad(), pk.Pad()));
+            }
+        }
+    }
+}
---
+++ Chaos.NaCl/Ed25519.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Chaos.NaCl.Internal.Ed25519Ref10;
+using System.Diagnostics.Contracts;
+
+namespace Chaos.NaCl
+{
+    public static class Ed25519
+    {
+        public static readonly int PublicKeySize = 32;
+        public static readonly int SignatureSize = 64;
+        public static readonly int ExpandedPrivateKeySize = 32 * 2;
+        public static readonly int PrivateKeySeedSize = 32;
+
+        public static bool Verify(ArraySegment<byte> signature, ArraySegment<byte> message, ArraySegment<byte> publicKey)
+        {
+            Contract.Requires<ArgumentException>(signature.Count == SignatureSize && publicKey.Count == PublicKeySize);
+
+            return Ed25519Operations.crypto_sign_verify(signature.Array, signature.Offset, message.Array, message.Offset, message.Count, publicKey.Array, publicKey.Offset);
+        }
+
+        public static bool Verify(byte[] signature, byte[] message, byte[] publicKey)
+        {
+            Contract.Requires<ArgumentNullException>(signature != null && message != null && publicKey != null);
+            Contract.Requires<ArgumentException>(signature.Length == SignatureSize && publicKey.Length == PublicKeySize);
+
+            return Ed25519Operations.crypto_sign_verify(signature, 0, message, 0, message.Length, publicKey, 0);
+        }
+
+        public static void Sign(ArraySegment<byte> signature, ArraySegment<byte> message, ArraySegment<byte> expandedPrivateKey)
+        {
+            Contract.Requires<ArgumentNullException>(signature.Array != null && message.Array != null && expandedPrivateKey.Array != null);
+            Contract.Requires<ArgumentException>(expandedPrivateKey.Count == ExpandedPrivateKeySize);
+
+            Ed25519Operations.crypto_sign(signature.Array, signature.Offset, message.Array, message.Offset, message.Count, expandedPrivateKey.Array, expandedPrivateKey.Offset);
+        }
+
+        public static byte[] Sign(byte[] message, byte[] expandedPrivateKey)
+        {
+            var signature = new byte[SignatureSize];
+            Sign(new ArraySegment<byte>(signature), new ArraySegment<byte>(message), new ArraySegment<byte>(expandedPrivateKey));
+            return signature;
+        }
+
+        public static byte[] PublicKeyFromSeed(byte[] privateKeySeed)
+        {
+            byte[] privateKey;
+            byte[] publicKey;
+            KeyPairFromSeed(out publicKey, out privateKey, privateKeySeed);
+            CryptoBytes.Wipe(privateKey);
+            return publicKey;
+        }
+
+        public static byte[] ExpandedPrivateKeyFromSeed(byte[] privateKeySeed)
+        {
+            byte[] privateKey;
+            byte[] publicKey;
+            KeyPairFromSeed(out publicKey, out privateKey, privateKeySeed);
+            CryptoBytes.Wipe(publicKey);
+            return privateKey;
+        }
+
+        public static void KeyPairFromSeed(out byte[] publicKey, out byte[] expandedPrivateKey, byte[] privateKeySeed)
+        {
+            Contract.Requires<ArgumentNullException>(privateKeySeed != null);
+            Contract.Requires<ArgumentException>(privateKeySeed.Length == PrivateKeySeedSize);
+
+            var pk = new byte[PublicKeySize];
+            var sk = new byte[ExpandedPrivateKeySize];
+
+            Ed25519Operations.crypto_sign_keypair(pk, 0, sk, 0, privateKeySeed, 0);
+            publicKey = pk;
+            expandedPrivateKey = sk;
+        }
+
+        public static void KeyPairFromSeed(ArraySegment<byte> publicKey, ArraySegment<byte> expandedPrivateKey, ArraySegment<byte> privateKeySeed)
+        {
+            Contract.Requires<ArgumentNullException>(publicKey.Array != null && expandedPrivateKey.Array != null && privateKeySeed.Array != null);
+            Contract.Requires<ArgumentException>(expandedPrivateKey.Count == ExpandedPrivateKeySize && privateKeySeed.Count == PrivateKeySeedSize);
+            Contract.Requires<ArgumentException>(publicKey.Count == PublicKeySize);
+
+            Ed25519Operations.crypto_sign_keypair(
+                publicKey.Array, publicKey.Offset,
+                expandedPrivateKey.Array, expandedPrivateKey.Offset,
+                privateKeySeed.Array, privateKeySeed.Offset);
+        }
+    }
+}
